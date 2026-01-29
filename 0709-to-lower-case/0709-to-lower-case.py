ec40b0cc0543e8d240fbe2cfcd9ccfd80151e8cc
kuldeep-p-a <kuldeepagarwala99@gmail.com>
2026-01-29 13:23:45 +0530

    Time: 0 ms (100%), Space: 12.5 MB (54.11%) - LeetHub

---
+++ 0709-to-lower-case/0709-to-lower-case.py
@@ -0,0 +1,8 @@
+class Solution(object):
+    def toLowerCase(self, s):
+        s_low = s.lower()
+        return s_low
+obj = Solution()
+print(obj.toLowerCase("Hello"))
+print(obj.toLowerCase("hello"))
+print(obj.toLowerCase("HeLlO"))
